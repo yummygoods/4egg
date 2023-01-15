5b6adbdf621752033495799f9f315834e0e5189e
yummygoods <melissa@yummygoods.com>
2023-01-15 16:21:38 -0500

    add annotations and mapping for CRUD

--- src/main/java/com/model/Track.java
+++ src/main/java/com/model/Track.java
@@ -3,23 +3,22 @@ package com.model;
 import jakarta.persistence.*;
 
 @Entity
+@Table(name="tracks", schema="4eggs")
 public class Track{
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id", nullable = false)
-    private Long id;
-
     private Integer ID;
 
     public Track() {
     }
 
-    public Long getId() {
-        return id;
+    public Integer getId() {
+        return ID;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public void setId(Integer id) {
+        this.ID = id;
     }
 
     public Integer getID() {
