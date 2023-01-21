25093549141fde59e2f41c7d043d418153462ecc
yummygoods <melissa@yummygoods.com>
2023-01-21 13:01:46 -0500

    form is working correctly now, yay

--- src/main/resources/static/frontEnd/build/js/capture-from-form.js
+++ src/main/resources/static/frontEnd/build/js/capture-from-form.js
@@ -4,7 +4,13 @@ let button = document.getElementById('button');
 button.addEventListener('click', function(event) {
                                      event.preventDefault();
                                      addFromForm();
+                                     reset();
+
+
                                  });
+
+
+
 
 
   // object/class to capture form input
@@ -24,7 +30,7 @@ function addFromForm() {
 let trackName = document.getElementById('trackName').value;
  console.log("this should print the value from user input in Name field:", trackName);
 let trackNotes = document.getElementById('trackNotes').value;
- console.log("this should print the value from user input in Notes field: ", + trackNotes);
+ console.log("this should print the value from user input in Notes field: ",  trackNotes);
 
 //instantiates new Track object and passes variables defined above as arguments into the constructor
   const trackFromForm = new Track(trackName, trackNotes);
@@ -32,12 +38,19 @@ let trackNotes = document.getElementById('trackNotes').value;
 
   console.log("this should print the new track object with values:", trackFromForm);
 
-  //reset the form to clear data once it has been stored in the variables
- //form.reset();
+  //resets the field values to empty strings
+
 
 }
 
-
+      function reset(){
+                                 //tells the dom to remove the user's text input
+                                     document.getElementById('trackName').value = '';
+                                     document.getElementById('trackNotes').value = '';
+                                     //resets the values stored in the variables
+                                      trackName = '';
+                                      trackNotes = '';
+                                 };
 /*
 
 //turns new track object into json and sends to server  post request, returns promise and parses the json into response object
