85e8d3666097d990512f22081d27bbb1e1ace084
yummygoods <melissa@yummygoods.com>
2023-01-22 12:05:40 -0500

    save before starting update and delete

---
+++ src/main/resources/static/frontEnd/build/js/populate-from-get-request.js
@@ -0,0 +1,15 @@
+
+
+function getAllTracks() {
+  fetch('http://localhost:8080/tracks/all')
+    .then(response => response.json())
+    .then(data => {
+      let element = document.getElementById('tracks');
+      for (let item of data) {
+        element.innerHTML += `<p>${item.name}: ${item.notes}</p>`;
+      }
+    })
+    .catch(error => console.log(error));
+}
+
+window.onload = getAllTracks();
